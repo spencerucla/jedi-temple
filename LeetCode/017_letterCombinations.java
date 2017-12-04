de30e02ccaf1722c7145755c43e5cafce8743e46
Spencer Sutterlin <spencerucla@gmail.com>
2017-12-03 16:28:10 -0800

    LC: 017. Letter Combos #2 cleanup

--- LeetCode/017_letterCombinations.java
+++ LeetCode/017_letterCombinations.java
@@ -1,14 +1,14 @@
 class Solution {
+    private static final String[] KEYS = { "0", "1", "abc", "def", "ghi", "jkl",
+                                           "mno", "pqrs", "tuv", "wxyz" };
+
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 1) {
-            return stuff(digits.charAt(0));
-        }
         List<String> tail = new ArrayList<String>(); tail.add("");
         if (digits.length() > 1)
             tail = letterCombinations(digits.substring(1));
         List<String> head = new ArrayList<String>(); head.add("");
         if (digits.length() > 0)
-            head = stuff(digits.charAt(0));
+            head = getKeys(digits.charAt(0));
         List<String> ret = new ArrayList<String>();
         for (String h : head) {
             for (String t : tail) {
@@ -19,27 +19,12 @@ class Solution {
         return ret;
     }
 
-    private List<String> stuff(char c) {
+    private List<String> getKeys(char c) {
         List<String> list = new ArrayList<String>();
-        switch (c) {
-            case '2':
-                list.add("a"); list.add("b"); list.add("c"); return list;
-            case '3':
-                list.add("d"); list.add("e"); list.add("f"); return list;
-            case '4':
-                list.add("g"); list.add("h"); list.add("i"); return list;
-            case '5':
-                list.add("j"); list.add("k"); list.add("l"); return list;
-            case '6':
-                list.add("m"); list.add("n"); list.add("o"); return list;
-            case '7':
-                list.add("p"); list.add("q"); list.add("r"); list.add("s"); return list;
-            case '8':
-                list.add("t"); list.add("u"); list.add("v"); return list;
-            case '9':
-                list.add("w"); list.add("x"); list.add("y"); list.add("z"); return list;
-            default:
-                list.add(""); return list;
+        String arr = KEYS[c - '0'];
+        for (int i = 0; i < arr.length(); i++) {
+            list.add(arr.charAt(i);
         }
+        return list;
     }
 }
