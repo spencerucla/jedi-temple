56c4c54d24449685c640c3b9825f96c62b5cf847
Spencer Sutterlin <spencerucla@gmail.com>
2017-12-23 17:17:07 -0800

    LC: maxProfit improvements

--- LeetCode/121_maxProfit.java
+++ LeetCode/121_maxProfit.java
@@ -1,17 +1,12 @@
 class Solution {
     // best time to buy and sell stock
     public int maxProfit(int[] prices) {
-        if (prices.length == 0) return 0;
-        int best = 0, low = prices[0], hi = prices[0];
+        int best = 0, low = Integer.MAX_VALUE;
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < low) {
+            if (prices[i] < low)
                 low = prices[i];
-                hi = prices[i];
-            }
-            if (prices[i] > hi)
-                hi = prices[i];
-            if (hi - low > best)
-                best = hi - low;
+            if (prices[i] - low > best)
+                best = prices[i] - low;
         }
         return best;
     }
