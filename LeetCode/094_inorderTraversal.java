006002beba3686303b6e736092131c83e8e1bf25
Spencer Sutterlin <spencerucla@gmail.com>
2018-01-27 15:46:09 -0800

    LC: Iterative inorder binary tree traversal

--- LeetCode/094_inorderTraversal.java
+++ LeetCode/094_inorderTraversal.java
@@ -9,6 +9,11 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
+        return inorderTraversalIter(root);
+        // return inorderTraversalRec(root);
+    }
+
+    public List<Integer> inorderTraversalRec(TreeNode root) {
         List<Integer> ret = new ArrayList<Integer>();
         traverseInOrder(root, ret);
         return ret;
@@ -20,5 +25,21 @@ class Solution {
         traverseInOrder(node.left, ret);
         ret.add(node.val);
         traverseInOrder(node.right, ret);
+    }
+
+    public List<Integer> inorderTraversalIter(TreeNode root) {
+        List<Integer> values = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            values.add(root.val);
+            root = root.right;
+        }
+        return values;
     }
 }
