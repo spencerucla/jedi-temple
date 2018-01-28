b9ba4dc7c20af0650db3353f5727c68b76422179
Spencer Sutterlin <spencerucla@gmail.com>
2018-01-28 14:36:20 -0800

    LC: simplify unique paths

--- LeetCode/062_uniquePaths.java
+++ LeetCode/062_uniquePaths.java
@@ -3,14 +3,17 @@ class Solution {
         if (m <= 1 || n <= 1) return 1;
         int[][] paths = new int[m][n];
         for (int i = 0; i < m; i++) {
-            paths[i][0] = 1;
-        }
-        for (int j = 0; j < n; j++) {
-            paths[0][j] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                paths[i][j] = paths[i-1][j] + paths[i][j-1];
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    paths[i][j] = 1;
+                } else {
+                    int sum = 0;
+                    if (i-1 >= 0)
+                        sum += paths[i-1][j];
+                    if (j-1 >= 0)
+                        sum += paths[i][j-1];
+                    paths[i][j] = sum;
+                }
             }
         }
         return paths[m-1][n-1];
