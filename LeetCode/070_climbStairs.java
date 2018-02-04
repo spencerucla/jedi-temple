1a96e6fc87af4590dba4bb7ff67e46daadf00009
Spencer Sutterlin <spencerucla@gmail.com>
2018-02-04 11:45:02 -0800

    LC: Cleanup some

--- LeetCode/070_climbStairs.java
+++ LeetCode/070_climbStairs.java
@@ -1,12 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
         if (n == 1) return 1;
-        int[] arr = new int[n+1];
-        arr[1] = 1;
-        arr[2] = 2;
-        for (int i = 3; i <= n; i++) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i++) {
             arr[i] = arr[i-1] + arr[i-2];
         }
-        return arr[n];
+        return arr[n-1];
     }
 }
--- LeetCode/083_deleteDuplicates.java
+++ LeetCode/083_deleteDuplicates.java
@@ -20,4 +20,17 @@ class Solution {
         curr.next = null;
         return head;
     }
+
+    // clearer
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode p = head;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return head;
+    }
 }
