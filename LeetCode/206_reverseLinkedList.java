da7fa7b1c2a76a2ac3d4f05b1d61851cdfb38919
Spencer Sutterlin <spencerucla@gmail.com>
2018-02-09 11:48:11 -0800

    LC: reverse linked list

---
+++ LeetCode/206_reverseLinkedList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseListIter(head);
+        // return reverseListRec(head, null);
+    }
+
+    private ListNode reverseListIter(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+
+    private ListNode reverseListRec(ListNode head, ListNode newHead) {
+        if (head == null) {
+            return newHead;
+        }
+        ListNode next = head.next;
+        head.next = newHead;
+        return reverseListRec(next, head);
+    }
+}
