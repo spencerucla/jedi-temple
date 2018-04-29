98905230accc1a463d639093b2198d30bf000cf6
Spencer Sutterlin <spencerucla@gmail.com>
2018-04-29 14:22:48 -0700

    LC: make nth ugly number faster

--- LeetCode/264_nthUglyNumber.java
+++ LeetCode/264_nthUglyNumber.java
@@ -1,16 +1,19 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> heap = new PriorityQueue<Long>();
-        heap.add(1l);
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+        int next2 = 2, next3 = 3, next5 = 5;
         for (int i = 1; i < n; i++) {
-            long curr = heap.poll();
-            if (!heap.contains(curr * 2))
-                heap.add(curr * 2);
-            if (!heap.contains(curr * 3))
-                heap.add(curr * 3);
-            if (!heap.contains(curr * 5))
-                heap.add(curr * 5);
+            int min = Math.min(Math.min(next2, next3), next5);
+            ugly[i] = min;
+            if (min == next2)
+                next2 = 2 * ugly[++index2];
+            if (min == next3)
+                next3 = 3 * ugly[++index3];
+            if (min == next5)
+                next5 = 5 * ugly[++index5];
         }
-        return heap.peek().intValue();
+        return ugly[n-1];
     }
 }
