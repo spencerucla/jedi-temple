1388cc79fbc0fed62c20c0463cc4c103fe68db4f
Spencer Sutterlin <spencerucla@gmail.com>
2022-02-14 13:50:32 -0800

    LC: improve LCP

--- LeetCode/014_longestCommonPrefix.java
+++ LeetCode/014_longestCommonPrefix.java
@@ -1,12 +1,11 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
-        for (int i = 1; i < strs.length && prefix.length() > 0; i++) {
-            boolean match = false;
-            while (prefix.length() > 0 && !match) {
-                match = prefix.equals(strs[i].substring(0, Math.min(prefix.length(), strs[i].length())));
-                if (!match) {
-                    prefix = prefix.substring(0, prefix.length()-1);
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) {
+                    return "";
                 }
             }
         }
