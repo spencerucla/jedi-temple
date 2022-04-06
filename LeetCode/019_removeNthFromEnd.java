a0716ecad996e63641a9dc22389e9c42388d6d49
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-05 18:20:34 -0700

    LC: algos 1 day 5 - two pointers / linkedlist

--- LeetCode/019_removeNthFromEnd.java
+++ LeetCode/019_removeNthFromEnd.java
@@ -27,4 +27,28 @@ class Solution {
         slow.next = slow.next.next;
         return ret.next;
     }
+
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        // move fast n places ahead of slow
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        // n == sz, return 2nd node as new head
+        if (fast == null) {
+            return head.next;
+        }
+
+        // now move them at same pace until fast at end
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
 }
