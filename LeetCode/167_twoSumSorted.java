719e548d3a0df481a7f9ccfcaa22352204bbaee7
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-05 17:03:14 -0700

    LC: Fix two sum sorted w/two pointers

--- LeetCode/167_twoSumSorted.java
+++ LeetCode/167_twoSumSorted.java
@@ -1,27 +1,16 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int len = numbers.length;
-        for (int i = 0; i < len; i++) {
-            int ret = search(numbers, target - numbers[i], i+1, len-1);
-            if (ret != -1) {
-                return new int[] { i+1, ret+1 };
+        int i = 0, j = len-1;
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target) {
+                return new int[] { i+1, j+1 };
+            } else if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else {
+                i++;
             }
         }
         return new int[] { -1, -1 };
-    }
-
-    private int search(int[] numbers, int target, int start, int end) {
-        int mid = (start + end) / 2;
-        if (target == numbers[mid]) {
-            return mid;
-        }
-        if (start >= end) {
-            return -1;
-        }
-        if (target > numbers[mid]) {
-            return search(numbers, target, mid+1, end); // right half
-        } else {
-            return search(numbers, target, start, mid); // left half
-        }
     }
 }
