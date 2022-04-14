daaa7aa09f7d38e9fee10af79655ae7525f50e52
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-14 11:08:25 -0700

    LC: max area island - remove visited

--- LeetCode/695_maxAreaIsland.java
+++ LeetCode/695_maxAreaIsland.java
@@ -1,28 +1,25 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         int maxArea = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (!visited[i][j]) {
-                    int area = search(grid, i, j, visited);
-                    maxArea = Math.max(maxArea, area);
-                }
+                int area = search(grid, i, j);
+                maxArea = Math.max(maxArea, area);
             }
         }
         return maxArea;
     }
 
-    private int search(int[][] grid, int sr, int sc, boolean[][] visited) {
+    private int search(int[][] grid, int sr, int sc) {
         if (sr < 0 || sr >= grid.length || sc < 0 || sc >= grid[sr].length
-                || grid[sr][sc] == 0 || visited[sr][sc])
+                || grid[sr][sc] == 0)
             return 0;
-        visited[sr][sc] = true;
+        grid[sr][sc] = 0;
         int area = 1;
-        area += search(grid, sr-1, sc, visited);
-        area += search(grid, sr+1, sc, visited);
-        area += search(grid, sr, sc-1, visited);
-        area += search(grid, sr, sc+1, visited);
+        area += search(grid, sr-1, sc);
+        area += search(grid, sr+1, sc);
+        area += search(grid, sr, sc-1);
+        area += search(grid, sr, sc+1);
         return area;
     }
 }
