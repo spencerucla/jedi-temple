abb3ed70522396169b4cf2081d33a36c3b0c6cf4
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-15 14:01:19 -0700

    LC: bull cows - improvement - 1 pass

--- LeetCode/299_bullCows.java
+++ LeetCode/299_bullCows.java
@@ -1,27 +1,19 @@
 class Solution {
     public String getHint(String secret, String guess) {
         int bullCount = 0;
+        int cowCount = 0;
         int[] digitCounts = new int[10];
-        String remainingGuess = "";
-
         for (int i = 0; i < secret.length(); i++) {
             if (secret.charAt(i) == guess.charAt(i)) {
                 bullCount++;
             } else {
+                if (digitCounts[guess.charAt(i) - '0'] > 0) cowCount++;
+                if (digitCounts[secret.charAt(i) - '0'] < 0) cowCount++;
                 digitCounts[secret.charAt(i) - '0']++;
-                remainingGuess += guess.charAt(i);
+                digitCounts[guess.charAt(i) - '0']--;
             }
         }
-
-        int cowCount = 0;
-        for (int i = 0; i < remainingGuess.length(); i++) {
-            char c = remainingGuess.charAt(i);
-            if (digitCounts[c - '0'] > 0) {
-                cowCount++;
-                digitCounts[c - '0']--;
-            }
-        }
-
-      	return "" + bullCount + "B" + cowCount + "C";
-   }
+      	return "" + bullCount + "A" + cowCount + "B";
+    }
 }
+
