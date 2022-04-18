e4d2d5d2e0fa450531eb2cce478e544fe89a7311
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-18 14:36:56 -0700

    LC: word break - dp, not better

--- LeetCode/139_wordBreak.java
+++ LeetCode/139_wordBreak.java
@@ -18,4 +18,18 @@ class Solution {
         cache.put(s, false);
         return false;
     }
+
+    public boolean wordBreakDP(String s, List<String> wordDict) {
+        boolean[] found = new boolean[s.length()+1];
+        found[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (found[j] && wordDict.contains(s.substring(j, i))) {
+                    found[i] = true;
+                    break;
+                }
+            }
+        }
+        return found[s.length()];
+    }
 }
