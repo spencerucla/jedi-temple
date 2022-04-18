9768f2241d97d7badb960b3d507cd804b7d63ecc
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-18 14:20:32 -0700

    LC: word break - improvement - memoize

--- LeetCode/139_wordBreak.java
+++ LeetCode/139_wordBreak.java
@@ -1,17 +1,21 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> d = new HashSet<>(wordDict);
-        return searchWords(s, d);
+        Map<String, Boolean> cache = new HashMap<>();
+        return searchWords(s, d, cache);
     }
 
-    private boolean searchWords(String s, Set<String> wordDict) {
+    private boolean searchWords(String s, Set<String> wordDict, Map<String, Boolean> cache) {
+        if (cache.containsKey(s)) return cache.get(s);
         if (wordDict.contains(s)) return true;
 
         for (int i = 1; i < s.length(); i++) {
-            if (wordDict.contains(s.substring(0, i)) && searchWords(s.substring(i), wordDict)) {
+            if (wordDict.contains(s.substring(0, i)) && searchWords(s.substring(i), wordDict, cache)) {
+                cache.put(s, true);
                 return true;
             }
         }
+        cache.put(s, false);
         return false;
     }
 }
