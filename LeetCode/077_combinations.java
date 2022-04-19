226e3dabb7ccafd9dff1aec66527443f2acbfe29
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-19 12:19:40 -0700

    LC: combinations improvement w/start

--- LeetCode/077_combinations.java
+++ LeetCode/077_combinations.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> list = new ArrayList<>();
-        dfs(n, k, new ArrayList<>(), list);
+        dfs(n, k, 1, new ArrayList<>(), list);
         return list;
     }
 
-    private void dfs(int n, int k, List<Integer> curr, List<List<Integer>> list) {
+    private void dfs(int n, int k, int start, List<Integer> curr, List<List<Integer>> list) {
         if (k == 0) {
             list.add(new ArrayList<>(curr));
             return;
         }
         // invariant: only add bigger #s than last of curr
-        for (int i = curr.isEmpty() ? 1 : curr.get(curr.size()-1)+1; i <= n; i++) {
+        for (int i = start; i <= n; i++) {
             curr.add(i);
-            dfs(n, k-1, curr, list);
+            dfs(n, k-1, i+1, curr, list);
             curr.remove(curr.size()-1);
         }
     }
