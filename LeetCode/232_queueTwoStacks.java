3283af72c7c6614aaf562294664319f97653142f
Spencer Sutterlin <spencerucla@gmail.com>
2022-04-28 11:38:44 -0700

    LC: queue w/2 stacks, more elegant

--- LeetCode/232_queueTwoStacks.java
+++ LeetCode/232_queueTwoStacks.java
@@ -3,34 +3,26 @@ class MyQueue {
     private Stack<Integer> in = new Stack<>();
     private Stack<Integer> out = new Stack<>();
 
-    public MyQueue() {
-        
-    }
-
     public void push(int x) {
         in.push(x);
     }
 
     public int pop() {
-        update();
+        peek();
         return out.pop();
     }
 
     public int peek() {
-        update();
-        return out.peek();
-    }
-
-    public boolean empty() {
-        return in.isEmpty() && out.isEmpty();
-    }
-
-    private void update() {
         if (out.isEmpty()) {
             while (!in.isEmpty()) {
                 out.push(in.pop());
             }
         }
+        return out.peek();
+    }
+
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
     }
 }
 
