769d454d1779297dee7da615aac1ffc1a86e8a4f
Spencer Sutterlin <spencerucla@gmail.com>
2022-08-14 16:41:37 -0700

    LC: Jump game ii improve

--- LeetCode/045_jumpGameII.java
+++ LeetCode/045_jumpGameII.java
@@ -1,14 +1,13 @@
 class Solution {
     public int jump(int[] nums) {
-        int[] numJumps = new int[nums.length];
-        Arrays.fill(numJumps, Integer.MAX_VALUE);
-        numJumps[0] = 0;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j <= nums[i]; j++) {
-                if (i+j < nums.length)
-                    numJumps[i+j] = Math.min(numJumps[i+j], numJumps[i]+1);
+        int jumps = 0, currFarthest = 0, currEnd = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            currFarthest = Math.max(currFarthest, i + nums[i]);
+            if (i == currEnd) {
+                jumps++;
+                currEnd = currFarthest;
             }
         }
-        return numJumps[nums.length-1];
+        return jumps;
     }
 }
